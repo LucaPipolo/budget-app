0a90025121a4a49be039329708569a141e92252c
Luca Pipolo <luca@somoscuatro.es>
2025-03-12 21:32:06 +0100

    build: setup Laravel Telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,10 +6,12 @@ use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AppPanelProvider;
 use App\Providers\FortifyServiceProvider;
 use App\Providers\JetstreamServiceProvider;
+use App\Providers\TelescopeServiceProvider;
 
 return [
     AppServiceProvider::class,
     AppPanelProvider::class,
     FortifyServiceProvider::class,
     JetstreamServiceProvider::class,
+    TelescopeServiceProvider::class,
 ];
