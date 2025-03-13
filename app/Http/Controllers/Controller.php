51c3e76e6eca852d13aedb5adcedbf33c7662703
Luca Pipolo <luca@somoscuatro.es>
2025-03-13 18:35:38 +0100

    build: add support for validates controller requests

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
 abstract class Controller
 {
-    // Preserve brace position.
+    use ValidatesRequests;
 }
