ec9f64187cf1dfc1becfb83077863561a45a7e6a
Luca Pipolo <luca@somoscuatro.es>
2025-04-02 15:58:17 +0200

    feat: add InvalidRelationshipsException
    We are adding this custom exception to handle cases in which an user is
    trying to assign an entity that does not belong to them (e.g. a team) to
    another entity (e.g. a merchant).

---
+++ app/Exceptions/InvalidRelationshipException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+use Throwable;
+
+class InvalidRelationshipException extends Exception
+{
+    public function __construct(?string $message = null, int $code = 422, ?Throwable $previous = null)
+    {
+        parent::__construct($message ?? 'Invalid Relationship', 422, $previous);
+        parent::__construct($message, $code);
+    }
+}
