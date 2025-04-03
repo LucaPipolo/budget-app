1038e419fc9e7310e79856cf26936f25c76fa127
Luca Pipolo <luca@somoscuatro.es>
2025-04-03 18:28:38 +0200

    fix(api/v1): add logo path validation to UpdateMerchantRequest

--- app/Http/Requests/Api/V1/Merchants/UpdateMerchantRequest.php
+++ app/Http/Requests/Api/V1/Merchants/UpdateMerchantRequest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Requests\Api\V1\Merchants;
 
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Storage;
 
 class UpdateMerchantRequest extends BaseMerchantRequest
 {
@@ -19,6 +20,20 @@ class UpdateMerchantRequest extends BaseMerchantRequest
             'data.attributes.name' => 'sometimes|string|min:3|max:255',
             'data.attributes.balance' => 'sometimes|integer|min:0',
             'data.attributes.teamId' => 'sometimes|uuid|exists:teams,id',
+            'data.attributes.logoPath' => [
+                'sometimes',
+                'nullable',
+                'string',
+                function (string $attribute, string $value, \Closure $fail): void {
+                    if ($value !== '' && ! Storage::disk('public')->exists($value)) {
+                        $fail("Field {$attribute} refers to a non existing file.");
+                    }
+
+                    if ($value !== '' && ! str_starts_with($value, 'merchants/')) {
+                        $fail("The {$attribute} should start with 'merchants/'.");
+                    }
+                },
+            ],
         ];
     }
 }
